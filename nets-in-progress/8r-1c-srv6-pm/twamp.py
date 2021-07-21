09a9cf2b19d33c5cd162faadab2fd273e99cab56
The ROSE project <rose_project@rose_project.org>
2021-07-21 18:07:09 +0200

    created packet sender e reflector

--- nets-in-progress/8r-1c-srv6-pm/twamp.py
+++ nets-in-progress/8r-1c-srv6-pm/twamp.py
@@ -41,3 +41,39 @@ class TWAMPTestResponse(Packet):
                     XByteField("MBZ",0),
                     ByteEnumField("ReceverControlCode", 0, {1: "Error - Invalid Message"}),
                     XByteField("SenderTTL",0)] #manca il padding
+
+class TWAMPTSessionSender(Packet):
+    name ="TWAMPSender"
+    fields_desc=[IntField("SequenceNumber",0),
+                    BitField("FirstPartTimestamp",0,32),
+                    BitField("SecondPartTimestamp",0,32),
+                    BitEnumField("S", 0, 1, {0: " no external synchronization",
+                                             1: "external synchronization"}),
+                    BitField("Z",0,1),
+                    BitField("Scale",0,6),
+                    BitField("Multiplier",0,8)] #manca il padding
+
+
+class TWAMPTSessionReflector(Packet):
+    name ="TWAMPReflector"
+    fields_desc=[IntField("SequenceNumber",0),
+                    BitField("FirstPartTimestamp",0,32),
+                    BitField("SecondPartTimestamp",0,32),
+                    BitEnumField("S", 0, 1, {0: " no external synchronization",
+                                             1: "external synchronization"}),
+                    BitField("Z",0,1),
+                    BitField("Scale",0,6),
+                    BitField("Multiplier",0,8),
+                    BitField("MBZ",0,16),
+                    BitField("FirstPartTimestampReceiver",0,32),
+                    BitField("SecondPartTimestampReceiver",0,32),
+                    IntField("SequenceNumberSender",0),
+                    BitField("FirstPartTimestampSender",0,32),
+                    BitField("SecondPartTimestampSender",0,32),
+                    BitEnumField("SSender", 0, 1, {0: " no external synchronization",
+                                             1: "external synchronization"}),
+                    BitField("ZSender ",0,1),
+                    BitField("ScaleSender",0,6),
+                    BitField("MultiplierSender",0,8),
+                    BitField("MBZ",0,16),
+                    ByteField("SenderTTL",255)] #manca il padding
